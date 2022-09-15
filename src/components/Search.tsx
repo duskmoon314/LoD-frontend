3eebedfa4fe9a5bf6934d4c7d5ae306feb2c106a
Campbell He <kp.campbell.he@duskmoon314.com>
2022-09-15 11:05:12 +0800

    feat: use facet to control search lang

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,12 +4,17 @@ import "@docsearch/css"
 const { DocSearch } =
   (docsearch as unknown as { default: typeof docsearch }).default || docsearch
 
-const Search: React.FC = () => {
+interface SearchProps {
+  lang?: string
+}
+
+const Search: React.FC<SearchProps> = ({ lang = "zh" }) => {
   return (
     <DocSearch
       appId="C3ROG2Z4EZ"
       indexName="duskmoon314"
       apiKey="6fd3ef15fabbd6e4589e820019157865"
+      searchParameters={{ facetFilters: [`lang:${lang}`] }}
     />
   )
 }
