ac9c7a4537578f53b96895adc5f8befbf09e4773
Campbell He <kp.campbell.he@duskmoon314.com>
2023-04-23 17:11:51 +0800

    feat(i18n): add blog namespace

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,5 +3,5 @@ import type { AstroI18nextConfig } from "astro-i18next/index"
 export default <AstroI18nextConfig>{
   defaultLocale: "zh",
   locales: ["zh", "en"],
-  namespaces: ["site", "home"]
+  namespaces: ["site", "home", "blog"]
 }
